89c404fa6898bdb0f4c9ea6f2a880fc38f99892c
ImShizer <68175695+ImShizer@users.noreply.github.com>
2023-12-18 02:19:45 +0200

    Work

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Symbol
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
